1376ad0156d13e426d009122e37ba01d1f98308e
Tung Dang <tungds@vimo.vn>
2023-12-09 14:26:24 +0700

    update

--- screens/category.js
+++ screens/category.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, FlatList } from 'react-native';
 import ProductItem from '../components/productItem';
+import axios from 'axios';
 
 export default function Category({ }) {
     const [products, setProducts] = useState([
@@ -34,6 +35,19 @@ export default function Category({ }) {
             price: "400k"
         }
     ]);
+
+    const getData = async ()=>{
+        try{
+            const data = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
+        }catch(err){
+            console.log("ERR : ", err);
+        }
+        
+    }
+
+    useEffect(()=>{
+        getData();
+    }, []);
 
     return (
         <View style={styles.container}>
